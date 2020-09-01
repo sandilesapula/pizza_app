911a4114a4f673352c494d2a39b4ff2fa70f8397
GIT Admin <davidsandilesapula@gmail.com>
2020-09-01 17:31:25 +0200

    2:03

--- server.js
+++ server.js
@@ -1,40 +1,54 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash');
+const { json } = require('express');
+const MongoDbStore = require('connect-mongo')(session)
 
-//Assets
+
+// Database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser : true, useCreateIndex : true, useUnifiedTopology : true, useFindAndModify : true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
+// Session store
+let mongoStore = new MongoDbStore({
+            mongooseConnection: connection,
+            collection: 'sessions'
+        })
+
+// Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
+}))
+
+app.use(flash())
+
+// Assets
 app.use(express.static('public'))
+app.use(express.json())
 
-
-
-//set Template engine
+// Set Template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
-//root
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-//cart
-app.get('/cart', (req, res) => {
-     res.render('customers/cart');
-});
-
-//login
-app.get('/login', (req, res) => {
-    res.render('auth/login');
-});
-
-//register
-app.get('/register', (req, res) => {
-    res.render('auth/register');
-});
-
+require('./routes/web.js')(app)
 
 
 
